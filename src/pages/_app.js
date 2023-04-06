7e46433390a4d4ca313cf7594befec8008e254ce
waliurrahman <mwaliurrahman@gmail.com>
2023-04-06 22:37:35 +0600

    Error of Providers and Next-Auth still there

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
 import '@/styles/globals.css'
 import Layout from '../components/Layout';
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }) {
-  return <Layout>
-    <Component {...pageProps} />
-    </Layout>
+export default function App({
+  Component, pageProps: { session, ...pageProps }
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps}/>
+      </Layout>
+    </SessionProvider>
+  )
 }
-
